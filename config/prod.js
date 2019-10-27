4c9ea6d972caa6221681518d52ea43339290aef3
frankiesastronautfund <grotton.2@buckeyemail.osu.edu>
2019-10-26 23:23:19 -0400

    change config vars

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 //configure env variables on heroku
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  spotifyClientID: process.env.SPOTIFY_CLIENT_ID,
+  spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
